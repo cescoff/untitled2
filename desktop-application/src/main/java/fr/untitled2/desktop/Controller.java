b55afcbf1a72ced3c7a463edcb346a56df78cb45
Corentin <corentin.escoffier@gmail.com>
2013-08-02 17:30:19 +0200

    Nouvelle application

--- desktop-application/src/main/java/fr/untitled2/desktop/Controller.java
+++ desktop-application/src/main/java/fr/untitled2/desktop/Controller.java
@@ -1,4 +1,23 @@
 package fr.untitled2.desktop;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.text.Text;
+
 public class Controller {
+
+    @FXML
+    private Label handleFileText;
+
+    @FXML
+    private ProgressBar imagePositionProgress;
+
+    @FXML protected void handleAddPositionToMyImages(ActionEvent actionEvent) {
+        imagePositionProgress.setVisible(true);
+        imagePositionProgress.setProgress(0.5);
+    }
+
+
 }
